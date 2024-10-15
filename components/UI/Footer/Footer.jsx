90cafee05e515b2fe5ae682895601f4c1b26f130
Dominik <dominik.jojczyk@gmail.com>
2024-10-15 20:31:04 +0200

    1.4

--- components/UI/Footer/Footer.jsx
+++ components/UI/Footer/Footer.jsx
@@ -1,13 +1,26 @@
+'use client'
 import Link from "next/link";
 import classes from "./Footer.module.css";
+import { FaArrowUp } from "react-icons/fa";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
+
+  // Funkcja do przewinięcia strony na samą górę
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Płynne przewijanie
+    });
+  };
 
   return (
     <footer className={classes.footer}>
       &copy; {currentYear} Barbara Piękoś. Projekt i realizacja{" "}
       <Link href={"https://dominikjojczyk.pl/"}>Dominik</Link>
+      <span onClick={scrollToTop} className={classes.arrow}>
+        <FaArrowUp />
+      </span>
     </footer>
   );
 }
