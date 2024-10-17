13286ec68c5560b3b2e1ccb2e297e0c4af49994f
Dominik <dominik.jojczyk@gmail.com>
2024-10-17 09:50:53 +0200

    1.2

--- components/Homepage/Baner.jsx
+++ components/Homepage/Baner.jsx
@@ -1,48 +1,68 @@
+"use client";
+import { motion } from "framer-motion";
 import Button from "../UI/Buttons/Button";
 import classes from "./Baner.module.css";
 import Image from "next/image";
 
 export default function Baner() {
   return (
-    <>
-      <section className={classes.baner__container}>
-        <h1>Barbara Piękoś</h1>
-        {/* Obraz dla wersji komputerowej */}
-        <div className={classes.image__container__desktop}>
-          <Image
-            src={"/baner.png"}
-            alt="Baner w postaci obrazu wykonanego przez autorkę Barbarę Piękoś"
-            width={100}
-            height={100}
-            layout="responsive"
-          />
-        </div>
+    <motion.section
+      className={classes.baner__container}
+      initial={{ opacity: 0 }} // Początkowy stan (niewidoczny)
+      animate={{ opacity: 1 }} // Animacja (fade-in)
+      transition={{ duration: 1.2 }} // Czas trwania animacji
+    >
+      {/* Animowany nagłówek */}
+      <motion.h1
+        initial={{ opacity: 0, y: -50 }} // Start (niewidoczny, przesunięty w górę)
+        animate={{ opacity: 1, y: 0 }} // Animacja (pojawienie i przesunięcie na miejsce)
+        transition={{ duration: 1.2, delay: 0.5 }} // Opóźnienie
+      >
+        Barbara Piękoś
+      </motion.h1>
 
-        {/* Obraz dla wersji mobilnej */}
-        <div className={classes.image__container__mobile}>
-          <Image
-            src={"/obrazy/3.jpeg"}
-            alt="Baner w postaci obrazu wykonanego przez autorkę Barbarę Piękoś"
-            width={100}
-            height={100}
-            layout="responsive"
-          />
-        </div>
-        <div className={classes.image__text__container}>
-          <Image
-            src={"/moja natura.png"}
-            width={100}
-            height={100}
-            alt="Odręczny napis 'moja natura'"
-            layout="responsive"
-          />
-        </div>
+      {/* Obraz dla wersji komputerowej */}
+      <motion.div
+        className={classes.image__container__desktop}
+        initial={{ opacity: 0, x: -100 }} // Start (niewidoczny, przesunięty w lewo)
+        animate={{ opacity: 1, x: 0 }} // Animacja (przesunięcie na miejsce)
+        transition={{ duration: 1.2, delay: 0.8 }} // Opóźnienie
+      >
+        <Image
+          src={"/baner.png"}
+          alt="Baner w postaci obrazu wykonanego przez autorkę Barbarę Piękoś"
+          width={100}
+          height={100}
+          layout="responsive"
+        />
+      </motion.div>
 
-        {/* <div className={classes.buttons__container}>
-          <Button link="#twórczość" text="Twórczość" />
-          <Button link="#wystawy" text="Wystawy" />
-        </div> */}
-      </section>
-    </>
+      {/* Obraz dla wersji mobilnej */}
+      <motion.div
+        className={classes.image__container__mobile}
+        initial={{ opacity: 0, x: -100 }} // Start (niewidoczny, przesunięty w lewo)
+        animate={{ opacity: 1, x: 0 }} // Animacja (przesunięcie na miejsce)
+        transition={{ duration: 1.2, delay: 1 }} // Opóźnienie
+      >
+        <Image
+          src={"/obrazy/3.jpeg"}
+          alt="Baner w postaci obrazu wykonanego przez autorkę Barbarę Piękoś"
+          width={100}
+          height={100}
+          layout="responsive"
+        />
+      </motion.div>
+
+      {/* Napis */}
+      <div className={classes.image__text__container}>
+        <Image
+          src={"/moja natura.png"}
+          width={100}
+          height={100}
+          alt="Odręczny napis 'moja natura'"
+          layout="responsive"
+        />
+      </div>
+    </motion.section>
   );
 }
